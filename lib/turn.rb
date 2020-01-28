65ace2747fafbc9b14ccd0cb0be7d87d939d257f
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-01-28 15:03:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,6 +31,7 @@ def turn(board)
         puts "Please enter 1-9:"
         user_input = gets.chomp.to_i
         index = input_to_index(user_input)
+        valid_move?(board, index)
     end
     move(board, index)
     display_board(board)
