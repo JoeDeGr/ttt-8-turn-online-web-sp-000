ca84a3dec416d33d6ed8d09990b92c24499babe1
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-01-28 14:36:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,11 +24,10 @@ def valid_move?(board, index)
  end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.chomp.to_i
-  index = input_to_index(user_input)
   while !(valid_move?(board, index)) do
     puts "Please enter 1-9:"
+    user_input = gets.chomp.to_i
+    index = input_to_index(user_input)
   end
   move(board)
   display_board(board)
